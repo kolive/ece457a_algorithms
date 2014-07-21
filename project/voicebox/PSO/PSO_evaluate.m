2615492af8748c82f01e981abe0e3ec68052f4fe
Michael Thiessen <mthiesse@uwaterloo.ca>
2014-07-20 20:13:12 -0400

    Optimize now takes ~0% of time

--- project/voicebox/PSO/PSO_evaluate.m
+++ project/voicebox/PSO/PSO_evaluate.m
@@ -1,5 +1,7 @@
-function [fitness,min_fitness,min_individual, count]=PSO_evaluate(position,k,N,D,L,var,x_max,fitness,y, fs, duration, giventags,Num_func,min_fitness, min_individual, count)
+function [fitness,min_fitness,min_individual, count]=PSO_evaluate(position,k,N,D,L,var,x_max,fitness,y, fs, duration, giventags,Num_func,min_fitness, min_individual, count, plotenable, figh, iteration)
 count = count + 1;
+iteration = N * iteration;
+giventags = [giventags; 1.1];
 for i=1:N
     for j=1:var
         temp=position(i,(j-1)*L+1:j*L);
@@ -32,12 +34,44 @@ for i=1:N
     individual.ta=0.396;    
     individual.gx=10 + ((1000 - 10)*X(5));
     individual.xn=1.995262 * X(6);
-    tags = vadsohn(y, fs, 'a', individual);
-    fitness(i,k) = vadOptimality(tags, giventags, duration, 0);
+    tags = [vadsohn(y, fs, 'a', individual); 1.1];
+    if (mod(iteration, 50) == 0)
+
+    end
+    
+    fitness(i,k) = vadOptimality(tags, giventags, duration, plotenable, figh, iteration);
     if fitness(i, k) < min_fitness
-        min_fitness = fitness(i, k);
+        min_fitness = fitness(i, k)
         min_individual = individual;
         count = 0;
+        
+                x1 = linspace(0, duration, size(tags,1));
+        x1 = x1';
+
+
+        x2 = linspace(0, duration, size(giventags,1));
+        x2 = x2';
+
+        %plot the two waveforms for comparison
+        figure(figh(1));
+        s(1) = subplot(3,1,3);
+        plot(s(1), x1, tags);
+        xlabel('Time (s)');
+        ylabel('Tag Value');
+        s(2) = subplot(3,1,2);
+        plot(s(2), x2, giventags);
+        title('Results of Vadsohn Analysis (bottom) vs. Given Tags (top)');
+        xlabel('Time (s)');
+        ylabel('Tag Value');
+        s(3) = subplot(3,1,1);
+        t=0:1/fs:(length(y)-1)/fs; %get the duration for plotting
+        plot(s(3), t, y);
+        title('Actual speech waveform');
+        xlabel('Time (s)');
+
+        saveas(figh(1), strcat('1Generation-', int2str(iteration), '.png'),'png');  
+        
     end
+    iteration = iteration + 1;
 end
 return
--- project/voicebox/vadOptimality.m
+++ project/voicebox/vadOptimality.m
@@ -15,22 +15,25 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
         figh(2) = figure;
     end
 
+    vsize = size(vtags,1);
+    rsize = size(rtags,1);
+    
     %create time chunk size for vtags
-    vchunksize = duration/size(vtags,1);
+    vchunksize = duration/rsize;
 
     %offset amount, e.g., how many vad tags per real tag?
-    ocount = floor(size(vtags,1)/size(rtags,1));
+    ocount = vsize/rsize;
 
     %some of the vtags won't be analyzed because #vtags not divisible by #rtags
     %this will introduce some analysis error.. may want to select which vtags
     %to skip in order to minimize this
     %right now, just the  vtags at the end chunks will be thrown out
-    analysisrange = ocount * size(rtags,1);
+    analysisrange = rsize;
 
     %amount of thrown out vtags, may introduce error because the ones we keep
     %dont line up perfectly with the rtags, though this is a small chunk of
     %time so error should be neglible
-    uad = duration - analysisrange * vchunksize;
+    uad = floor((1/ocount) * vsize);
 
     %initialize error types
     fec = 0;
@@ -39,30 +42,29 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
     nds = 0;
 
     %for plotting
-    vtimeplot = linspace(0, duration, size(vtags,1));
-    rtimeplot = linspace(0, duration, size(rtags,1));
-    fecplot = zeros(1, size(vtags,1));
-    mscplot = zeros(1, size(vtags,1));
-    overplot = zeros(1, size(vtags,1));
-    ndsplot = zeros(1, size(vtags,1));
+    vtimeplot = linspace(0, duration, vsize);
+    rtimeplot = linspace(0, duration, rsize);
+    fecplot = zeros(1, vsize);
+    mscplot = zeros(1, vsize);
+    overplot = zeros(1, vsize);
+    ndsplot = zeros(1, vsize);
 
     %calculate the VAD Error Rate
     %detect FEC and MSC
-    i = 1;
     fecmode = 1;
     mscmode = 0;
-    for j=1:analysisrange,
+    for j=1:rsize,
        %to calculate FEC, determine if we've recently transitioned from a
        %no-speech to a speech zone and add up the duration of time that we're
        %mis-detecting speech
-       if(i > 1 && rtags(i-1) == 0 && rtags(i) == 1 && fecmode == 0)
+       if(j > 1 && rtags(j-1) == 0 && rtags(j) == 1 && fecmode == 0)
           %set FEC detection mode on
           fecmode = 1;
        end
-       if((fecmode == 1 || mscmode == 1) && (rtags(i) == vtags(j) || rtags(i) == 0))
+       if((fecmode == 1 || mscmode == 1) && (rtags(j) == vtags(floor(j*ocount)) || rtags(j) == 0))
           %we've either left the speech zone or detection started so no longer
           %FEC and now into MSC mode
-          if(rtags(i) == 0)
+          if(rtags(j) == 0)
               fecmode = 0;
               mscmode = 0;
           elseif(fecmode == 1)
@@ -74,7 +76,7 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
 
        %if fecmode or mscmode is on and there is an error,
        %classify it as the correct error
-       if((mscmode == 1 || fecmode == 1) && vtags(j) ~= rtags(i))
+       if((mscmode == 1 || fecmode == 1) && vtags(floor(j*ocount)) ~= rtags(j))
            if(mscmode == 1)
                msc = msc + vchunksize;
                mscplot(j) = 1;
@@ -83,30 +85,24 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
                fecplot(j) = 1;
            end
        end
-
-       if(mod(j,ocount) == 0)
-           %time to increment the real chunk counter
-           i = i + 1;
-       end
     end
     %calculate the FTR Error Rate
     %detect OVER and NDS
-    i = 1;
     overmode = 0;
     ndsmode = 0;
-    for j=1:analysisrange,
+    for j=1:rsize,
        %to calculate OVER, determine if we've recently transitioned from a
        %speech to a no-speech zone and add up the duration of time that we're
        %mis-detecting speech
 
-       if(i > 1 && rtags(i-1) == 1 && rtags(i) == 0 && overmode == 0)
+       if(j > 1 && rtags(j-1) == 1 && rtags(j) == 0 && overmode == 0)
           %set FEC detection mode on
           overmode = 1;
        end
-       if((overmode == 1 || ndsmode == 1) && (rtags(i) == vtags(j) || rtags(i) == 1))
+       if((overmode == 1 || ndsmode == 1) && (rtags(j) == vtags(floor(j*ocount)) || rtags(j) == 1))
           %we've either entered a speech zone or detection stopped so no longer
           %OVER and now into NDS region
-          if(rtags(i) == 1)
+          if(rtags(j) == 1)
             overmode = 0;
             ndsmode = 0;
           elseif(overmode == 1)
@@ -118,7 +114,7 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
 
        %if overmode or ndsmode is on and there's an error, classify it as the
        %correct error
-       if((overmode == 1 || ndsmode == 1) && rtags(i) ~= vtags(j))
+       if((overmode == 1 || ndsmode == 1) && rtags(j) ~= vtags(floor(j*ocount)))
            if(ndsmode == 1)
                nds = nds + vchunksize;
                ndsplot(j) = 1;
@@ -126,11 +122,6 @@ function [optimality, breakdown, uad, figh]=vadOptimality(vtags, rtags, duration
                over = over + vchunksize;
                overplot(j) = 1;
            end
-       end
-
-       if(mod(j,ocount) == 0)
-           %time to increment the real chunk counter
-           i = i + 1;
        end
     end
 
