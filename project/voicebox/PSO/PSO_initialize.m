fe71277e956abc008bf54035595419a1e29f6c78
Michael Thiessen <mthiesse@uwaterloo.ca>
2014-07-26 09:12:06 -0400

    parallel PSO

--- project/voicebox/PSO/PSO_initialize.m
+++ project/voicebox/PSO/PSO_initialize.m
@@ -1,7 +1,7 @@
 function [N,K,D,L,var,w_max,w_min,c1,c2,position,p_best,g_best,fitness,p_best_fit,...
     Num_func,Min_Max_flag,Gl_Lo_flag]=PSO_initialize
-N = 34;    % N is the number of the particles
-K = 36;  %K is the number of iteration
+N = 66;    % N is the number of the particles
+K = 66;  %K is the number of iteration
 var = 6; % var is number of variables
 L = 30 ; % L is the lenght for each variable
 D = L*var; % D is the dimension of each particle
