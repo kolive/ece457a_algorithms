e20e833a85783232cbe51cfa714bf0eff14a69d4
Michael Thiessen <mthiesse@uwaterloo.ca>
2014-07-29 18:51:01 -0400

    update readme

--- project/voicebox/PSO/PSO_initialize.m
+++ project/voicebox/PSO/PSO_initialize.m
@@ -3,7 +3,7 @@ function [D,L,var,w_max,w_min,c1,c2,position,p_best,g_best,fitness,p_best_fit,..
 var = 6; % var is number of variables
 L = 30 ; % L is the lenght for each variable
 D = L*var; % D is the dimension of each particle
-w_min=0.1;w_max=1.2;c1=1.8;c2=1.8;% w is the inertia factor and c1 & c2 are learning factors
+w_min=0.1;w_max=0.6;c1=1.8;c2=1.8;% w is the inertia factor and c1 & c2 are learning factors
 position = rand(N,D)>0.5; % Generates initial population
 fitness=0;
 p_best = rand(N,D)>0.5;
