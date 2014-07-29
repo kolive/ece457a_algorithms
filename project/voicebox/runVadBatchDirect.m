87d08eac6d4e7538dfa1d62977629f546867372a
Kyle Olive <kolive@uwaterloo.ca>
2014-07-29 19:42:27 -0400

    parallel comment

--- project/voicebox/runVadBatchDirect.m
+++ project/voicebox/runVadBatchDirect.m
@@ -1,3 +1,20 @@
+%%%%%%%
+%  Author: Kyle
+%
+%	runVadBatchDirect takes the following parameters:
+%
+%		y - the WAV audio file data
+%		fs - the sampling frequency of the WAV file
+%		duration - the length of the audio clip
+%		giventags - the ground truth
+%		population - the set of solutions to evaluate, can be of size 1..N
+%
+%		RunVadBatchDirect then runs the evaluation and optimality calculations
+%		in parallel. The user is responsible for settingup the matlabpool. 
+%
+%		Returns the optimalities of the clients. 
+%
+%%%%%%%
 function [batchoptimality] = runVadBatchDirect(y, fs, duration, giventags, population)
     
     %vadsohn with default parameters on y, fs
